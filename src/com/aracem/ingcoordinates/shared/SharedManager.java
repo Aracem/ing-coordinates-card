3e57bc54250c9c87f80ea54dc2a11e0100ac9e25
Marcos Trujillo <marcostruji@gmail.com>
2013-12-30 17:13:51 +0100

    Added save and check password methos to SharedManager

--- src/com/aracem/ingcoordinates/shared/SharedManager.java
+++ src/com/aracem/ingcoordinates/shared/SharedManager.java
@@ -1,12 +1,15 @@
 
 package com.aracem.ingcoordinates.shared;
 
+import com.aracem.ingcoordinates.utils.Sha1Utils;
+
 import android.content.Context;
 import android.content.SharedPreferences;
 
 public class SharedManager {
     private static final String SHARED_NAME = "ingcoordinatescard";
     private static final String KEY_READY = "ready";
+    private static final String KEY_PASS = "pass";
 
     private final SharedPreferences mPreferences;
 
@@ -21,4 +24,30 @@ public class SharedManager {
         return mPreferences.getBoolean(KEY_READY, false);
     }
 
+    /**
+     * Saves a password into SharedPreferences
+     * 
+     * @param pass
+     */
+    public void savePassword(String pass) {
+        if (pass != null && !"".equals(pass)) {
+            pass = Sha1Utils.generateSHA1toString(pass);
+            if (!"".equals(pass)) {
+                mPreferences.edit().putString(KEY_PASS, pass);
+            }
+        }
+    }
+
+    /**
+     * @return true when the password is equals than the saved password in
+     *         shared preferences
+     * @param password
+     */
+    public boolean checkPassword(String password) {
+        if (password == null || "".equals(password) || !mPreferences.contains(KEY_PASS))
+            return false;
+
+        return mPreferences.getString(KEY_PASS, "")
+                .equals(Sha1Utils.generateSHA1toString(password));
+    }
 }
